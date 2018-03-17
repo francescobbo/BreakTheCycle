c038ad72148b7771e9776bf4a4271b4a21ff3146
Francesco Boffa <fra.boffa@gmail.com>
2018-03-17 20:02:36 +0000

    AASDSGWROFWERFOWRNGE%ONG

--- app/services/api_client.rb
+++ app/services/api_client.rb
@@ -1,32 +1,60 @@
 class ApiClient
-  HOST = 'https://santander.openbankproject.com/'
-  USER ="https://santander.openbankproject.com/obp/v3.0.0/users"
+  BASE_ENDPOINT ="https://santander.openbankproject.com/obp/v3.0.0/"
 
   attr_reader :token
 
-  def initialize
-    @username = Rails.application.credentials.obp.username
-    @password = Rails.application.credentials.obp.password
-    @consumer_key = Rails.application.credentials.obp.consumer_key
+  def initialize(username, password)
+    @username = username
+    @password = password
+    @consumer_key = Rails.application.credentials.obp[:consumer_key]
   end
 
-  def authentication
-    post('my/logins/direct', nil, {
-      'Authorization': "DirectLogin username=\"#{@username}\", password=\"#{@password}\", consumer_key=\"#{@consumer_key}\""
-    })
+  def current_user
+    get('users/current', nil)
   end
 
-  def token
-    @token ||= JSON.parse(authentication)
+  def get_accounts
+    get('banks/santander.01.uk.sanuk/accounts-held', nil)
+  end
+
+  def get_transactions(account_id)
+    get("my/banks/santander.01.uk.sanuk/accounts/#{account_id}/transactions")
   end
 
   private
+
+  def token
+    @token ||= authentication[:token]
+  end
 
   def default_headers
     { 'Content-type': 'application/json' }
   end
 
-  def post(endpoint, data, headers)
-    RestClient.post("#{HOST}/#{endpoint}", data, default_headers.merge(headers))
+  def auth_headers
+    { 'Authorization': "DirectLogin token=\"#{token}\""}
+  end
+
+  def handle(response)
+    if response.code / 100 == 2
+      JSON.parse(response).with_indifferent_access
+    else
+      raise StandardError, "What the hell!"
+    end
+  end
+
+  def get(endpoint, data = nil, headers = {})
+    headers = default_headers.merge(auth_headers).merge(headers).merge(params: data)
+    handle RestClient.get("#{BASE_ENDPOINT}/#{endpoint}", headers)
+  end
+
+  def post(endpoint, data = nil, headers = {})
+    handle RestClient.post("#{BASE_ENDPOINT}/#{endpoint}", data, default_headers.merge(auth_headers).merge(headers))
+  end
+
+  def authentication
+    handle RestClient.post('https://santander.openbankproject.com/my/logins/direct', nil, default_headers.merge({
+      'Authorization': "DirectLogin username=\"#{@username}\", password=\"#{@password}\", consumer_key=\"#{@consumer_key}\""
+    }))
   end
 end
